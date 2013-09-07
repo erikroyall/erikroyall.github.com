d309a90a95515eaf399601563ba499ab447754e3
Erik Royall <erikroyall@hotmail.com>
2013-09-07 09:25:40 +0530

    Add new 'tools'

---
+++ js/tool-html5-page.js
@@ -0,0 +1,12 @@
+(function (w, d) {
+  var $ = function (x) {
+        return document.getElementById(x);
+      }
+    , form = $("gen")
+    , page {
+        title: $("title").value,
+        charset: $("charset").value
+      };
+
+  
+}(window, window.document));
